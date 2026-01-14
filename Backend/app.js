badc6eb469a275ed2230c91b55b5a841a911dc36
Ananya10-Coder <ananya.gmk@gmail.com>
2026-01-15 04:29:19 +0530

    user registration route created

---
+++ Backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+app.use(cors());
+
+const connectToDb = require('./db/db');
+connectToDb();
+const userRoutes = require('./routes/user.routes');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+});
+
+app.use('/users', userRoutes);
+
+module.exports = app;
