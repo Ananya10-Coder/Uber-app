2a12e523a4b84032bb02f6582cdb84744b37e19e
Ananya10-Coder <ananya.gmk@gmail.com>
2026-01-15 04:50:13 +0530

    user login route created

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -6,9 +6,16 @@ const userController = require('../controllers/user.controller');
 router.post('/register', [
     body('email').isEmail().withMessage('Invalid Email'),
     body('fullname.firstname').isLength({min:3}).withMessage('First name must be at least 3 characters long'),
-    body('password').isLength({min: 5}).withMessage('First name must be at least 3 characters long')
+    body('password').isLength({min: 5}).withMessage('Password must be at least 3 characters long')
 ],
     userController.registerUser
+)
+
+router.post('login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min: 5}).withMessage('password must be at least 3 characters long')
+],
+    userController.loginUser
 )
 
 module.exports = router;
